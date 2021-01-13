7d263c14db53c15d9ab11bdf0fc8fd73ff5fc1a6
Paul Nilsson <paul.nilsson@smokeball.com>
2021-01-13 16:22:08 +1100

    remove some login modules whick were wrappers

--- src/login/index.ts
+++ src/login/index.ts
@@ -1,3 +1,5 @@
 export { startSRP } from "./start-srp";
-export { refresh } from "./refresh";
-export { setupDevice } from "./setup-device";
+export {
+  initiateRefreshToken as refresh,
+  confirmDevice as setupDevice,
+} from "../cognito/operations";
